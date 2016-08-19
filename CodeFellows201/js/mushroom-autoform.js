1c280a013091a3e65d5a02846c19489b04952682
Gloria Guy <totallygloria@gmail.com>
2016-08-18 17:42:51 -0700

    Once I got the html generation function working, it was a quick hop to having the entire page be dynamically created based on the currentMushroom array. The page now works as a dichotomous key!
    Need to find gill, pore, and teeth images. Also, still need text.

--- CodeFellows201/js/mushroom-autoform.js
+++ CodeFellows201/js/mushroom-autoform.js
@@ -15,12 +15,27 @@ var mushroom_types = ["caps", "morels", "trumpets", "puffballs", "corals", "shel
 
 // Specific mushrooms per type
 var m_hash = {
-    caps: ["shaggymane", "kingbolete" ,"hedgehog"],
+    caps: ["gills", "pores", "teeth"],
+    gills: ["shaggymane"],
+    pores: ["kingbolete"],
+    teeth: ["hedgehog"],
     morels: ["blackmorel"],
     trumpets: ["bluechanterelle", "chanterelle"],
     puffballs: ["commonpuffball"],
     corals: ["cauliflower"],
     shelves: ["chickenofthewoods", "oyster"]
+}
+
+var description_hash = {
+    caps: "Cap & Stem",
+    gills: "Underside of Cap is Gilled",
+    pores: "Underside of Cap has Pores",
+    teeth: "Underside of Cap has Fine Tooth-like Appendages",
+    morels: "Caps has Pits & Ridges",
+    trumpets: "Trumpet or Vase-Shaped",
+    puffballs: "Ball-like or Ball-on-Stem",
+    corals: "Coral or Cauliflower-Like",
+    shelves: "Shelf or Cup-Like"
 }
 
 // Instruction text
@@ -29,7 +44,7 @@ var capInstructions = "Does the underside of the cap have gills, pores (like foa
 
 // Form information
 var form = document.getElementById('mushroom_selector');
-var options = form.elements.allShrooms;
+//var options = form.elements.allShrooms;
 var selection = '';
 
 // Session Set-up
@@ -38,14 +53,54 @@ var selection = '';
 var currentMushrooms = mushroom_types;
 localStorage.setItem('currentMushrooms',JSON.stringify(currentMushrooms));
 
+// Create the mushroom webform
+function populateForm(mlist) {
+
+    for(var i=0; i < mlist.length; i++) {
+        // Make a new label to hold the image and p text
+        var newLabel = document.createElement('label');
+        newLabel.setAttribute('class', 'unselected');
+        newLabel.setAttribute('id', mlist[i]);
+        newId = mlist[i];
+
+        // Make a new input type for the form data
+        var newInput = document.createElement('input');
+        newInput.setAttribute('type', 'radio');
+        newInput.setAttribute('name', mlist);
+        newInput.setAttribute('value', mlist[i]);
+
+        // Make a new image, set src
+        var newImg = document.createElement('img');
+        var pic = "img/" + mlist[i] + ".jpg";
+        newImg.setAttribute('src', pic);
+
+        // Make a new paragraph, set text
+        var newPara = document.createElement('p');
+        var content = currentMushrooms
+        var newText = document.createTextNode(mlist[i]);
+        newPara.appendChild(newText);
+
+        // Make the input, image, and paragraph children of the label
+        newLabel.appendChild(newInput);
+        newLabel.appendChild(newImg);
+        newLabel.appendChild(newPara);
+
+        // Append the new label to the form, add interactivity
+        document.getElementById('mushroom_selector').appendChild(newLabel);
+        document.getElementById(newId).addEventListener("onmouseover", mushroomOver(newId));
+        document.getElementById(newId).addEventListener("onmouseout", mushroomOut(newId));
+        document.getElementById(newId).addEventListener("onclick", mushroomClick(newId));
+    }
+}
+
 
 // Functions for mouse events
 function makeInteractive() {
-    for(var i=0; i < mushroom_types.length; i++) {
+    for(var i=0; i < currentMushrooms.length; i++) {
         // Attach a listener to each label in the form
-        document.getElementById(mushroom_types[i]).addEventListener("onmouseover", mushroomOver(mushroom_types[i]));
-        document.getElementById(mushroom_types[i]).addEventListener("onmouseout", mushroomOut(mushroom_types[i]));
-        document.getElementById(mushroom_types[i]).addEventListener("onclick", mushroomClick(mushroom_types[i]));
+        document.getElementById(currentMushrooms[i]).addEventListener("onmouseover", mushroomOver(currentMushrooms[i]));
+        document.getElementById(currentMushrooms[i]).addEventListener("onmouseout", mushroomOut(currentMushrooms[i]));
+        document.getElementById(currentMushrooms[i]).addEventListener("onclick", mushroomClick(currentMushrooms[i]));
     };
     form.addEventListener('click', function(event) { event.preventDefault(); mformSubmit(); } );
 }
@@ -70,14 +125,12 @@ function mushroomClick(mtype) {
 function mformSubmit() {
     fadeOut();
     setTimeout(setupMushrooms, 1000);
-
 }
 
 // When the window is ready, set up event listeners and form, then respond to clicks
 window.onload = function() {
-    if (localStorage.getItem('currentMushrooms') != '' && localStorage.getItem('currentMushrooms') != null) {
-        currentMushrooms = JSON.parse(localStorage.getItem('currentMushrooms'));
-    };
+    currentMushrooms = JSON.parse(localStorage.getItem('currentMushrooms'));
+    populateForm(currentMushrooms);
     makeInteractive();
 }
 
@@ -93,14 +146,13 @@ function fadeOut() {
 }
 
 function setupMushrooms() {
-    var lenSelection = (m_hash[selection]).length;
-    removeOld((currentMushrooms.length - lenSelection));
+    removeOld();
     insertNew();
-    currentMushrooms = selection;
+    currentMushrooms = m_hash[selection];
     localStorage.setItem('currentMushrooms',JSON.stringify(currentMushrooms));
 }
 
-function removeOld(numToRemove) {
+function removeOld() {
     for(var i=0; i < currentMushrooms.length; i++) {
         if (currentMushrooms[i] != selection) {
             document.getElementById(currentMushrooms[i]).className = 'remove';
@@ -109,33 +161,7 @@ function removeOld(numToRemove) {
 }
 
 function insertNew() {
-    newMushrooms = m_hash[selection];
-    var pic = "img/" + newMushrooms[0] + ".jpg";
-    document.getElementById(selection).children[1].src = pic;
-    document.getElementById(selection).children[2].innerHTML = newMushrooms[0];
-    document.getElementById(selection).id = newMushrooms[0];
-    document.getElementById("instructions").innerHTML = "Does your mushroom look like these " + selection + "?";
-
-    if (newMushrooms.length > 1) {
-        for(var i=1; i < newMushrooms.length; i++) {
-            // Make a new label to hold the image and p text
-            var newLabel = document.createElement('label');
-            newLabel.setAttribute('class', 'unselected');
-            newLabel.setAttribute('id', newMushrooms[i]);
-            // Make a new image, set src
-            var newImg = document.createElement('img');
-            var pic = "img/" + newMushrooms[i] + ".jpg";
-            newImg.setAttribute('src', pic);
-            // Make a new paragraph, set text
-            var newPara = document.createElement('p');
-            var newText = document.createTextNode(newMushrooms[i]);
-            newPara.appendChild(newText);
-            // Make the image and paragraph children of the label
-            newLabel.appendChild(newImg);
-            newLabel.appendChild(newPara);
-            // Append the new label to the form
-            var element = document.getElementById('mushroom_selector');
-            element.appendChild(newLabel);
-        }
-    };
+    populateForm(m_hash[selection]);
 }
+
+
