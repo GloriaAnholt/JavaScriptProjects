a269681614438f0730575426f324747bb1b11cc9
Gloria Guy <totallygloria@gmail.com>
2016-08-23 21:00:28 -0700

    Worked all day to re-do the page as an html list where elements are displayed or not based on the session value for currentMushrooms. Basic functionality of eventListeners works correctly, onclick has far more of the page logic in it than it used to. The 'viewall' option has it's own function for when clicked which resets the page. Some of the fade/appear functionality around caps doesn't work correctly yet, but otherwise the page functions more or less as intended.

--- CodeFellows201/js/mushroom-autoform.js
+++ CodeFellows201/js/mushroom-autoform.js
@@ -10,78 +10,15 @@
 // as a form. This continues until one mushroom type is left, which then
 // displays that/those mushroom(s).
 
-// Mushrooms data structure
-var m_hash = {
-    alltypes: ["caps", "morels", "trumpets", "puffballs", "corals", "shelves"],
-    caps: ["gills", "pores", "teeth"],
-    gills: ["shaggymane"],
-    pores: ["kingbolete"],
-    teeth: ["hedgehog"],
-    morels: ["blackmorel"],
-    trumpets: ["bluechanterelle", "chanterelle"],
-    puffballs: ["commonpuffball"],
-    corals: ["cauliflower"],
-    shelves: ["chickenofthewoods", "oyster"]
-}
-
-var description_hash = {
-    caps: "Cap & Stem",
-    gills: "Underside of Cap is Gilled",
-    pores: "Underside of Cap has Pores",
-    teeth: "Underside of Cap has Fine Tooth-like Appendages",
-    morels: "Caps has Pits & Ridges",
-    trumpets: "Trumpet or Vase-Shaped",
-    puffballs: "Ball-like or Ball-on-Stem",
-    corals: "Coral or Cauliflower-Like",
-    shelves: "Shelf or Cup-Like"
-}
 
 // Instruction text
 var mInstructions = "Select the outline of your mushroom to get started.";
 var capInstructions = "Does the underside of the cap have gills, pores (like foam), or teeth?";
 
 // Form information
-var form = document.getElementById('mushroom_selector');
 var selection = '';
 var currentMushrooms = [];
 
-// Create the mushroom webform
-function populateForm(mlist) {
-
-    for(var i=0; i < mlist.length; i++) {
-        // Make a new label to hold the image and p text
-        var newLabel = document.createElement('label');
-        newLabel.setAttribute('class', 'unselected');
-        newLabel.setAttribute('id', mlist[i]);
-        newId = mlist[i];
-
-        // Make a new input type for the form data
-        var newInput = document.createElement('input');
-        newInput.setAttribute('type', 'radio');
-        newInput.setAttribute('name', mlist);
-        newInput.setAttribute('value', mlist[i]);
-
-        // Make a new image, set src
-        var newImg = document.createElement('img');
-        var pic = "img/" + mlist[i] + ".jpg";
-        newImg.setAttribute('src', pic);
-
-        // Make a new paragraph, set text
-        var newPara = document.createElement('p');
-        var content = currentMushrooms
-        var newText = document.createTextNode(mlist[i]);
-        newPara.appendChild(newText);
-
-        // Make the input, image, and paragraph children of the label
-        newLabel.appendChild(newInput);
-        newLabel.appendChild(newImg);
-        newLabel.appendChild(newPara);
-
-        // Append the new label to the form, add interactivity
-        document.getElementById('mushroom_selector').appendChild(newLabel);
-        makeInteractive(newLabel.id);
-    }
-}
 
 // Functions for mouse events
 function makeInteractive(mtype) {
@@ -120,36 +57,12 @@ window.onload = function() {
     populateForm(currentMushrooms);
 }
 
-// All the interactions functionality
-function resetForm() {
-    var formData = document.getElementById('mushroom_selector');
-    while ( formData.lastChild ) {
-        formData.removeChild( formData.lastChild );
-    };
-    currentMushrooms = m_hash['alltypes'];
-    sessionStorage.setItem('currentMushrooms',JSON.stringify(currentMushrooms));
-    populateForm(currentMushrooms);
-}
+
 
 function fadeOut() {
     for(var i=0; i < currentMushrooms.length; i++) {
         if (document.getElementById(currentMushrooms[i]).className != 'selected') {
            document.getElementById(currentMushrooms[i]).style.opacity = 0;
-        }
-    };
-}
-
-function setupMushrooms() {
-    removeOld();
-    populateForm(m_hash[selection]);
-    currentMushrooms = m_hash[selection];
-    sessionStorage.setItem('currentMushrooms',JSON.stringify(currentMushrooms));
-}
-
-function removeOld() {
-    for(var i=0; i < currentMushrooms.length; i++) {
-        if (currentMushrooms[i] != selection) {
-            document.getElementById(currentMushrooms[i]).className = 'remove';
         }
     };
 }
---
+++ CodeFellows201/js/mushroom-list.js
@@ -0,0 +1,164 @@
+// Code Fellows 201: Final Website Project for 301 Application
+// https://gloriaanholt.github.io/JavaScriptProjects/CodeFellows201/index.html
+// @totallygloria
+// 2016.08.11
+
+// From the main page, there are six selectable mushroom-types.
+// The grid of types serve as a js form: when the user clicks on
+// a type of mushroom, the main page serves back a sub-selection
+// page with a new set of relevant mushrooms to select from, also
+// as a form. This continues until one mushroom type is left, which then
+// displays that/those mushroom(s).
+
+
+// data sctructure
+var m_hash = {
+    alltypes: ["caps", "morels", "trumpets", "puffballs", "corals", "shelves"],
+    caps: ["gills", "pores", "teeth"],
+    gills: ["shaggymane"],
+    pores: ["kingbolete" ],
+    teeth: ["hedgehog"],
+    morels: ["blackmorel"],
+    trumpets: ["chanterelle", "bluechanterelle", "blackchanterelle"],
+    puffballs: ["commonpuffball"],
+    corals: ["cauliflower"],
+    shelves: ["chickenofthewoods", "oyster"]
+};
+
+// Instruction text
+// var mInstructions = "Select the outline of your mushroom to get started.";
+// var capInstructions = "Does the underside of the cap have gills, pores (like foam), or teeth?";
+
+// Form information
+var selection = '';
+var currentMushrooms = [];
+
+
+// Functions for mouse events
+function makeInteractive() {
+    var elemList = document.body.getElementsByTagName('li');
+    for (var i = 0; i < elemList.length; i++) {
+        elemList[i].addEventListener("mouseover", mushroomOver);
+        elemList[i].addEventListener("mouseout", mushroomOut);
+        if (elemList[i].id != "alltypes") {
+            elemList[i].addEventListener("click", mushroomClick);
+        } else {
+            elemList[i].addEventListener("click", resetPage);
+        }
+    }
+}
+
+function mushroomOver() {
+    if (this.className == 'unselected') {
+        this.className = 'active'
+    }
+}
+
+function mushroomOut() {
+    if (this.className == 'active') {
+        this.className = 'unselected'
+    }
+}
+
+function resetPage() {
+    var totalList = document.getElementById('mushroom_selector');
+    var elemList = totalList.getElementsByTagName('li');
+    var startingSet = m_hash["alltypes"];
+    for (var i=1; i < elemList.length; i++) {
+        elemList[i].className = 'remove';
+        }
+    currentMushrooms = ["alltypes"];
+    for (var i=0; i < startingSet.length; i++) {
+        document.getElementById(startingSet[i]).className = 'unselected';
+        document.getElementById(startingSet[i]).style.opacity = 100;
+        currentMushrooms.push(startingSet[i]);
+        }
+    sessionStorage.setItem('currentMushrooms',JSON.stringify(currentMushrooms));
+}
+
+function mushroomClick() {
+    this.className = 'selected';
+    selection = this.id;
+
+    // Reset the session data
+    if (selection == "gills" || selection == "pores" || selection == "teeth") {
+        currentMushrooms = ["alltypes", "caps", selection];
+    } else {
+        currentMushrooms = ["alltypes", selection];
+    }
+    for (var i=0; i < m_hash[selection].length; i++) {
+        currentMushrooms.push(m_hash[selection][i]);
+    }
+    sessionStorage.setItem('currentMushrooms',JSON.stringify(currentMushrooms));
+
+    // Change the displayed mushrooms to match selection
+    fadeOut();
+    setTimeout(removeOld, 800);
+    setTimeout(showChildren, 810);
+}
+
+
+/* When the window is ready, set up event listeners, then respond to clicks */
+window.onload = function() {
+
+    makeInteractive();
+
+    // This needs to change what is displayed on load, right now it doesn't do anything
+    if (sessionStorage.getItem('currentMushrooms') == [] || sessionStorage.getItem('currentMushrooms') === null) {
+        // If there's nothing in current, put the master list in it
+        currentMushrooms.push("alltypes");
+        var startingSet = m_hash["alltypes"];
+        for (var i=0; i < startingSet.length; i++) {
+            currentMushrooms.push(startingSet[i]);
+            document.getElementById(startingSet[i]).className = 'unselected';
+            document.getElementById(startingSet[i]).style.opacity = 100;
+        }
+        sessionStorage.setItem('currentMushrooms',JSON.stringify(currentMushrooms));
+    } else {
+        currentMushrooms = JSON.parse(sessionStorage.getItem('currentMushrooms'));
+        var totalList = document.getElementById('mushroom_selector');
+        var elemList = totalList.getElementsByTagName('li');
+        for (var i=0, j=0; i < elemList.length; i++) {
+            if (elemList[i].id == currentMushrooms[j]) {
+                elemList[i].className = 'unselected';
+                elemList[i].style.opacity = 100;
+                j++;
+            } else {
+                elemList[i].className = 'remove';
+            }
+        }
+    }
+};
+
+
+
+function fadeOut() {
+// Takes all of the selected item's siblings and fades them to zero
+    var elem = document.getElementById('alltypes').nextElementSibling;
+    while (elem) {
+        if (elem.className != 'selected') {
+            elem.style.opacity = 0;
+        }
+        elem = elem.nextElementSibling;
+    }
+}
+
+function removeOld() {
+// Takes all of the selected item's siblings and sets their display to none
+    var elem = document.getElementById('alltypes').nextElementSibling;
+    while (elem) {
+        if (elem.className != 'selected') {
+            elem.className = 'remove';
+        }
+        elem = elem.nextElementSibling;
+    }
+}
+
+function showChildren() {
+// Takes all of the selected item's children and sets them to be visible
+    var children = m_hash[selection];
+    for (var i=0; i < children.length; i++) {
+        document.getElementById(children[i]).className = 'unselected';
+        document.getElementById(children[i]).style.opacity = 100;
+    }
+}
